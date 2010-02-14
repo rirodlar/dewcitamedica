9bd752e60e0bf077e35a4bb319485b7518136c5f
kielsaenz <kielsaenz@82247850-1900-11df-85bb-b9a83fee2cb1>
2010-02-14 00:40:52 +0000

    Primera plantilla - Vacia

---
+++ CitasMedicas/src/java/pe/com/citasmedicas/model/Medico.java
@@ -0,0 +1,53 @@
+package pe.com.citasmedicas.model;
+
+// <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+// #[regen=yes,id=DCE.E8E49E72-CB03-9425-7634-5B701B5AFED0]
+// </editor-fold> 
+public class Medico {
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // #[regen=yes,id=DCE.B8FF6E8A-922F-E8C1-962C-576134DDADF4]
+    // </editor-fold> 
+    private Integer medicoId;
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // #[regen=yes,id=DCE.3A02B96F-22BF-15CE-0D96-9450D5DBE03E]
+    // </editor-fold> 
+    private String nombre;
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // #[regen=yes,id=DCE.BBC4C6D3-827F-916D-D148-D71922E7A46B]
+    // </editor-fold> 
+    public Medico () {
+    }
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // #[regen=yes,regenBody=yes,id=DCE.0798661C-4F93-B3BA-F66E-73005AA3D6D9]
+    // </editor-fold> 
+    public Integer getMedicoId () {
+        return medicoId;
+    }
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // #[regen=yes,regenBody=yes,id=DCE.29DC75CB-D613-2CFD-2A08-DE6A7D8196A8]
+    // </editor-fold> 
+    public void setMedicoId (Integer val) {
+        this.medicoId = val;
+    }
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // #[regen=yes,regenBody=yes,id=DCE.BBF8B68C-A6AD-C280-99EA-AA69906372C6]
+    // </editor-fold> 
+    public String getNombre () {
+        return nombre;
+    }
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // #[regen=yes,regenBody=yes,id=DCE.C4C3917D-95DC-6D67-16F7-B21368539984]
+    // </editor-fold> 
+    public void setNombre (String val) {
+        this.nombre = val;
+    }
+
+}
+
