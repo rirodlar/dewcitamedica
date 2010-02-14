7a7bf9eb4cb5697bb1b845e0ccd161acce9a4c67
kielsaenz <kielsaenz@82247850-1900-11df-85bb-b9a83fee2cb1>
2010-02-14 01:39:59 +0000

    Modelo v1.0

--- trunk/CitasMedicas/src/java/pe/com/citasmedicas/model/Cita.java
+++ trunk/CitasMedicas/src/java/pe/com/citasmedicas/model/Cita.java
@@ -1,94 +1,100 @@
 package pe.com.citasmedicas.model;
 
-// <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-
-import java.util.Date;
-
-// #[regen=yes,id=DCE.50C75D37-0695-232E-13D1-2D569BEB0CB1]
-// </editor-fold> 
+/**
+ *
+ * @author dew - Grupo 04
+ */
 public class Cita {
 
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-    // #[regen=yes,id=DCE.59B5670A-A9B3-3E28-4EC8-62D311A83800]
-    // </editor-fold> 
     private Integer citaId;
-
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-    // #[regen=yes,id=DCE.BE1B7E66-8253-613D-0554-39EADFCD0344]
-    // </editor-fold> 
-    private Date fechaCita;
-
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-    // #[regen=yes,id=DCE.ED042563-2326-71C1-9368-12C6B9288C89]
-    // </editor-fold> 
     private String estado;
-
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-    // #[regen=yes,id=DCE.6BA1B4E2-7E46-9D8A-F214-1E531B49935F]
-    // </editor-fold> 
     private String diagnostico;
+    private Paciente paciente;
+    private Medico medico;
+    private Horario horario;
 
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-    // #[regen=yes,id=DCE.45B6A8E1-63E0-BE09-39AD-033265792BB9]
-    // </editor-fold> 
-    public Cita () {
-    }
-
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-    // #[regen=yes,regenBody=yes,id=DCE.9A928A7D-EA09-E238-0557-62F7663FEC13]
-    // </editor-fold> 
-    public Integer getCitaId () {
+    /**
+     * @return the citaId
+     */
+    public Integer getCitaId() {
         return citaId;
     }
 
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-    // #[regen=yes,regenBody=yes,id=DCE.216BD661-563B-8705-3C0B-777EB2443852]
-    // </editor-fold> 
-    public void setCitaId (Integer val) {
-        this.citaId = val;
+    /**
+     * @param citaId the citaId to set
+     */
+    public void setCitaId(Integer citaId) {
+        this.citaId = citaId;
     }
 
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-    // #[regen=yes,regenBody=yes,id=DCE.70B37EAD-C394-9128-C103-51615D9ECBF7]
-    // </editor-fold> 
-    public String getDiagnostico () {
-        return diagnostico;
-    }
-
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-    // #[regen=yes,regenBody=yes,id=DCE.65FA5488-76B2-B7D2-52A7-9414AD3EC491]
-    // </editor-fold> 
-    public void setDiagnostico (String val) {
-        this.diagnostico = val;
-    }
-
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-    // #[regen=yes,regenBody=yes,id=DCE.8FBD8A01-B02F-79D1-FA96-B9C908F1A1FA]
-    // </editor-fold> 
-    public String getEstado () {
+    /**
+     * @return the estado
+     */
+    public String getEstado() {
         return estado;
     }
 
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-    // #[regen=yes,regenBody=yes,id=DCE.42A74454-28BD-457B-5884-3496043FA6C8]
-    // </editor-fold> 
-    public void setEstado (String val) {
-        this.estado = val;
+    /**
+     * @param estado the estado to set
+     */
+    public void setEstado(String estado) {
+        this.estado = estado;
     }
 
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-    // #[regen=yes,regenBody=yes,id=DCE.D5ECB3E8-8240-08AF-B213-BEA4182251A1]
-    // </editor-fold> 
-    public Date getFechaCita () {
-        return fechaCita;
+    /**
+     * @return the diagnostico
+     */
+    public String getDiagnostico() {
+        return diagnostico;
     }
 
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-    // #[regen=yes,regenBody=yes,id=DCE.19F7EF13-4B12-A1F1-A506-80E60D1D43C2]
-    // </editor-fold> 
-    public void setFechaCita (Date val) {
-        this.fechaCita = val;
+    /**
+     * @param diagnostico the diagnostico to set
+     */
+    public void setDiagnostico(String diagnostico) {
+        this.diagnostico = diagnostico;
     }
 
+    /**
+     * @return the paciente
+     */
+    public Paciente getPaciente() {
+        return paciente;
+    }
+
+    /**
+     * @param paciente the paciente to set
+     */
+    public void setPaciente(Paciente paciente) {
+        this.paciente = paciente;
+    }
+
+    /**
+     * @return the medico
+     */
+    public Medico getMedico() {
+        return medico;
+    }
+
+    /**
+     * @param medico the medico to set
+     */
+    public void setMedico(Medico medico) {
+        this.medico = medico;
+    }
+
+    /**
+     * @return the horario
+     */
+    public Horario getHorario() {
+        return horario;
+    }
+
+    /**
+     * @param horario the horario to set
+     */
+    public void setHorario(Horario horario) {
+        this.horario = horario;
+    }
 }
 
---
+++ trunk/CitasMedicas/src/java/pe/com/citasmedicas/model/Usuario.java
@@ -0,0 +1,142 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pe.com.citasmedicas.model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author dew - Grupo 04
+ */
+public class Usuario {
+
+    private Integer usuarioId;
+    private String username;
+    private String password;
+    private String nombre;
+    private String apellidoPaterno;
+    private String apellidoMaterno;
+    private Date fechaNacimiento;
+    private String nroDocumento;
+
+    /**
+     * @return the usuarioId
+     */
+    public Integer getUsuarioId() {
+        return usuarioId;
+    }
+
+    /**
+     * @param usuarioId the usuarioId to set
+     */
+    public void setUsuarioId(Integer usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+
+    /**
+     * @return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the apellidoPaterno
+     */
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    /**
+     * @param apellidoPaterno the apellidoPaterno to set
+     */
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    /**
+     * @return the apellidoMaterno
+     */
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    /**
+     * @param apellidoMaterno the apellidoMaterno to set
+     */
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    /**
+     * @return the fechaNacimiento
+     */
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    /**
+     * @param fechaNacimiento the fechaNacimiento to set
+     */
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    /**
+     * @return the nroDocumento
+     */
+    public String getNroDocumento() {
+        return nroDocumento;
+    }
+
+    /**
+     * @param nroDocumento the nroDocumento to set
+     */
+    public void setNroDocumento(String nroDocumento) {
+        this.nroDocumento = nroDocumento;
+    }
+
+    /**
+     * @return el nombre completo;
+     */
+    public String getNombreCompleto(){
+        return getApellidoPaterno() + " " + getApellidoMaterno() + ", " + getNombre();
+    }
+}
