171eb34d2cd57955d80930cc0de28fed64b55afb
kielsaenz <kielsaenz@82247850-1900-11df-85bb-b9a83fee2cb1>
2010-02-20 10:57:03 +0000

    Reserva Cita - v 1.7

--- CitasMedicas/src/java/pe/com/citasmedicas/accion/LoginAccion.java
+++ CitasMedicas/src/java/pe/com/citasmedicas/accion/LoginAccion.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package pe.com.citasmedicas.accion;
 
 import java.io.IOException;
@@ -18,31 +17,40 @@ import pe.com.citasmedicas.service.UsuarioService;
  *
  * @author rSaenz
  */
-public class LoginAccion implements Accion{
+public class LoginAccion implements Accion {
 
     private String vista;
 
     @Override
     public boolean ejecutar(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // Se recuperan los parametros del jsp
-        String username = request.getParameter("username");
-        String password = request.getParameter("password");
-        // Se obtiene el usuario
-        UsuarioService usuarioService = new UsuarioService();
-        Usuario usuario = usuarioService.getUsuarioPorUsername(username);
-        // Se verifica que exista el usuario y que coincida el password
-        if (usuario != null && usuario.getPassword().equals(password)) {
+        String argumento = request.getParameter("__ARGUMENT");
+        if (argumento.equals("validar")) {
+            // Se recuperan los parametros del jsp
+            String username = request.getParameter("username");
+            String password = request.getParameter("password");
+            // Se obtiene el usuario
+            UsuarioService usuarioService = new UsuarioService();
+            Usuario usuario = usuarioService.getUsuarioPorUsername(username);
+            // Se verifica que exista el usuario y que coincida el password
+            if (usuario != null && usuario.getPassword().equals(password)) {
+                if (request.isRequestedSessionIdValid()) {
+                    HttpSession tempSession = request.getSession(false);
+                    tempSession.invalidate();
+                    System.out.println("La sesión estaba activa");
+                }
+                HttpSession session = request.getSession(true);
+                session.setAttribute("usuario", usuario);
+                vista = "/home.jsp";
+            } else {
+                request.setAttribute("error", "El usuario y/o la contraseña son incorrectas.");
+                vista = "/loguin.jsp";
+            }
+        } else {
             if (request.isRequestedSessionIdValid()) {
                 HttpSession tempSession = request.getSession(false);
                 tempSession.invalidate();
-                System.out.println("la sesión estaba activa");
+                System.out.println("Se invalido la sesión");
             }
-            HttpSession session = request.getSession(true);
-            session.setAttribute("usuario", usuario);
-            vista = "/home.jsp";
-        } else {
-            request.setAttribute("error", "El usuario y/o la contraseña son incorrectas.");
-            vista = "/loguin.jsp";
         }
         return true;
     }
@@ -51,5 +59,4 @@ public class LoginAccion implements Accion{
     public String getVista() {
         return vista;
     }
-
 }
