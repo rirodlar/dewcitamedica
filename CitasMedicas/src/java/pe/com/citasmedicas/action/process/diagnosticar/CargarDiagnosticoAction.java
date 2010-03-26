3c14904d228cc66f3629ee4a78e8471381f86721
kielsaenz <kielsaenz@82247850-1900-11df-85bb-b9a83fee2cb1>
2010-03-26 20:42:09 +0000

    consultar cita medico

--- CitasMedicas/src/java/pe/com/citasmedicas/action/process/diagnosticar/CargarDiagnosticoAction.java
+++ CitasMedicas/src/java/pe/com/citasmedicas/action/process/diagnosticar/CargarDiagnosticoAction.java
@@ -1,12 +1,15 @@
 package pe.com.citasmedicas.action.process.diagnosticar;
 
 import com.opensymphony.xwork2.conversion.annotations.Conversion;
+import com.opensymphony.xwork2.conversion.annotations.TypeConversion;
+import javax.servlet.http.HttpSession;
 import org.apache.struts2.config.Namespace;
 import org.apache.struts2.config.ParentPackage;
 import org.apache.struts2.config.Result;
 import org.apache.struts2.config.Results;
 import org.apache.struts2.dispatcher.ServletDispatcherResult;
 import pe.com.citasmedicas.action.BaseAction;
+import pe.com.citasmedicas.model.Cita;
 import pe.com.citasmedicas.service.CitaService;
 /**
  *
@@ -21,11 +24,22 @@ import pe.com.citasmedicas.service.CitaService;
 public class CargarDiagnosticoAction extends BaseAction {
     
     private CitaService citaService;
+    private Integer txtCitaSelect;
 
     @Override
     public String execute() {
         try {
-            System.out.println("param: " + request.getAttribute("txtCitaSelect"));
+            Cita cita = null;
+
+            validarAutenticacion(request);
+            // Variables
+            HttpSession sesion = request.getSession();
+
+            //Obtener cita seleccionada
+            cita = citaService.getCitaPorId(txtCitaSelect);
+
+            sesion.setAttribute("cita", cita);
+
             return SUCCESS;
         } catch (Exception ex) {
             ex.printStackTrace();
@@ -40,6 +54,14 @@ public class CargarDiagnosticoAction extends BaseAction {
 
     public void setCitaService(CitaService citaService) {
         this.citaService = citaService;
+    }
+
+    public Integer getTxtCitaSelect() {
+        return txtCitaSelect;
+    }
+    
+    public void setTxtCitaSelect(Integer txtCitaSelect) {
+        this.txtCitaSelect = txtCitaSelect;
     }
     
 }
