87ade47e4c45a923c9110b2f4794ff29690e8a65
kielsaenz <kielsaenz@82247850-1900-11df-85bb-b9a83fee2cb1>
2010-04-08 19:30:17 +0000

    javadoc

--- trunk/CitasMedicas/src/java/pe/com/citasmedicas/action/LogoutAction.java
+++ trunk/CitasMedicas/src/java/pe/com/citasmedicas/action/LogoutAction.java
@@ -3,12 +3,17 @@ package pe.com.citasmedicas.action;
 import org.apache.struts2.config.Result;
 
 /**
- *
+ * Esta clase contiene los métodos utilizados la pantalla Logout
  * @author dew - Grupo 04
  */
 @Result(name="success", value="/logout.jsp")
 public class LogoutAction extends BaseAction {
 
+    /**
+     * Método encargado de ejecutar la acción solicitada
+     * @return estado exito
+     * @throws Exception
+     */
     @Override
     public String execute() throws Exception {
         request.getSession().invalidate();
