c616170621a28dfa1ca83a646bc6691a9ed6bd05
kielsaenz <kielsaenz@82247850-1900-11df-85bb-b9a83fee2cb1>
2010-04-08 19:39:35 +0000

    javadoc

--- CitasMedicas/src/java/pe/com/citasmedicas/action/converter/DateConverter.java
+++ CitasMedicas/src/java/pe/com/citasmedicas/action/converter/DateConverter.java
@@ -8,11 +8,18 @@ import java.util.Map;
 import org.apache.struts2.util.StrutsTypeConverter;
 
 /**
- *
+ * Esta clase contiene los métodos utilizados para manejar conversión de fechas en Struts
  * @author dew - Grupo 04
  */
 public class DateConverter extends StrutsTypeConverter {
 
+    /**
+     * Convierte una o más cadenas a una clase especificada
+     * @param context Acción del contexto
+     * @param values Valores de cadena a ser convertidos, tales como los enviados por el formulario HTML
+     * @param toClass Clase a la cual se convertirá
+     * @return Fecha obtenida desde cadena
+     */
     @Override
     public Object convertFromString(Map context, String[] values, Class toClass) {
         if (values != null && values.length > 0 &&
@@ -27,6 +34,12 @@ public class DateConverter extends StrutsTypeConverter {
         return null;
     }
 
+    /**
+     * Convierte un objeto especificado a cadena
+     * @param context Acción del contexto
+     * @param o Objeto a ser convertido
+     * @return Cadena convertida,  en este caso a formato dia/mes/anio
+     */
     @Override
     public String convertToString(Map context, Object o) {
         if (o instanceof Date) {
