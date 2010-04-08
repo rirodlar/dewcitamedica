b0575a944186f8c27ae4e6694a6f6cebd98aa689
kielsaenz <kielsaenz@82247850-1900-11df-85bb-b9a83fee2cb1>
2010-04-08 19:18:13 +0000

    comentarios

--- CitasMedicas/src/java/pe/com/citasmedicas/action/BaseAction.java
+++ CitasMedicas/src/java/pe/com/citasmedicas/action/BaseAction.java
@@ -7,17 +7,27 @@ import pe.com.citasmedicas.filter.SeguridadUtil;
 import pe.com.citasmedicas.exception.SeguridadException;
 
 /**
- *
  * @author dew - Grupo 04
+ * Action base del cual extienden los demás Action
  */
 public class BaseAction extends ActionSupport implements ServletRequestAware{
-
+    
     protected HttpServletRequest request;
 
+    /**
+     * Establece el request a la clase
+     * @param request
+     */
+    @Override
     public void setServletRequest(HttpServletRequest request) {
         this.request = request;
     }
 
+    /**
+     * Método encargado de validar la autenticación del usuario
+     * @param request
+     * @throws SeguridadException
+     */
     protected void validarAutenticacion(HttpServletRequest request) throws SeguridadException{
         if(!SeguridadUtil.estaAutenticado(request))
             throw new SeguridadException();
